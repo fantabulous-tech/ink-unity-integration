f8f1613f8395a0ce70794b070e4f0eb19b747f8f
Fantabulous <fan@fantabulous.tech>
2020-04-12 20:20:45 +1200

    Add story path test files

---
+++ Assets/Story Path Test/PathTest.ink
@@ -0,0 +1,27 @@
+-> start
+
+=== start
+This is at the start. #1
+
+This is the line before the first choice. #2
+
++ [continue1]
++ [continue2]
+
+- This is after the first choice. #3
+This is the line before the second choice. #4
+
++ [continue]
+
+- This before the tunnel. #5
+
+-> tunnel ->
+
++ [continue]
+
+This line is at the END. #6
+-> END
+
+=== tunnel
+Tunnel before a choice. How do I get a path for this!? #1
+->->
---
+++ Assets/Story Path Test/StoryPathTester.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Ink.Runtime;
+using UnityEngine;
+
+public class StoryPathTester : MonoBehaviour {
+	[SerializeField] private TextAsset m_StoryJSON;
+	
+	private void Start() {
+		Story story = new Story (m_StoryJSON.text);
+		while (story.canContinue) {
+			story.Continue();
+			string storyPath = story.state.currentPathString;
+			string knotName = storyPath != null ? storyPath.Substring(0, storyPath.IndexOf(".")) : "";
+			string firstTag = story.currentTags.Any() ? int.Parse(story.currentTags.First()).ToString("00") : "--no tag--";
+			string lineID = knotName + "." + firstTag;
+			string info = $"LineID {lineID} (path {storyPath}): {story.currentText.Trim()}";
+
+			if (storyPath == null) {
+				Debug.LogError($"<color=red>MISSING PATH</color>: {info}", m_StoryJSON);
+			} else {
+				Debug.Log(info, m_StoryJSON);
+			}
+
+			if (story.currentChoices.Any()) {
+				story.ChooseChoiceIndex(0);
+			}
+		}
+	}
+}
